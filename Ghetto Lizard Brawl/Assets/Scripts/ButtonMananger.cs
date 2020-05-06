4dd0eeb30082ce72809947c5d938ccf2a412bcbb
Connor <c_young97@live.com>
2020-05-06 12:09:34 +1000

    Code Commenting
    Not necessary

--- Ghetto Lizard Brawl/Assets/Scripts/ButtonMananger.cs
+++ Ghetto Lizard Brawl/Assets/Scripts/ButtonMananger.cs
@@ -6,10 +6,9 @@ using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Description:	Manages button behaviour
-/// Requirements:	Lizard
 /// Author(s):		Connor Young, Reyhan Rishard
 /// Date created:	04/05/20
-/// Date modified:	04/05/20
+/// Date modified:	06/05/20
 /// </summary>
 
 public class ButtonMananger : MonoBehaviour
